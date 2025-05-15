a00f816e82382b8e26391bd39f8c450a467244d0
Vishal Thakur <vishalthakur5862@gmail.com>
2025-05-15 12:09:25 +0530

    changed phone to phoneNumber for code Code Consistency

--- Backend/src/utils/phoneToken.js
+++ Backend/src/utils/phoneToken.js
@@ -4,14 +4,14 @@ dotenv.config();
 
 const SECRET = process.env.TEMP_VERIFICATION_SECRET;
 
-export function createPhoneToken(phone) {
-  return jwt.sign({ phone }, SECRET, { expiresIn: "10m" });
+export function createPhoneToken(phoneNumber) {
+  return jwt.sign({ phoneNumber }, SECRET, { expiresIn: "10m" });
 }
 
 export function verifyPhoneToken(token) {
   try {
     const decoded = jwt.verify(token, SECRET);
-    return decoded.phone;
+    return decoded.phoneNumber;
   } catch (err) {
     return null;
   }
